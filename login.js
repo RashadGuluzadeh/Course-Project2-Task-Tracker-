74534bd6e5ecd89dca277d419a67726651f6eded
Rashad <rashadquluzade@gmail.com>
2022-04-15 16:01:03 +0400

    Project

--- login.js
+++ login.js
@@ -1,26 +1,59 @@
-var input = document.querySelector('#inp-element')
-var btn = document.querySelector('.add-data')
-var showlist = document.querySelector('.list')
-var remove =  document.querySelector('.cls')
-var paragraph = document.querySelector('.list p')
-var clsform = document.querySelector('#cls-form')
+var input = document.querySelector("#inp-element");
+var btn = document.querySelector(".add-data");
+var showlist = document.querySelector(".list");
+var remove = document.querySelector(".cls");
+var paragraph = document.querySelector(".list p");
+var clsform = document.querySelector("#cls-form");
+var inpdata = document.querySelector('.inp-data')
+
+
+input.addEventListener("keyup", (e) => {
+  if (e.keyCode == 13) {
+    if(input.value.length == 0){
+      alert("Enter the task")
+      clsform.style.display = 'block'
+      inpdata.style.border = '1px solid #C4C4C4'
+    }
+    else  {
+      const list = document.createElement("div");
+      list.classList.add("todolist");
+      const addli = document.createElement("li");
+      addli.classList.add("showli");
+      addli.innerHTML = input.value;
+      list.appendChild(addli);
+      const deletebtn = document.createElement("button");
+      deletebtn.classList.add("remove-btn");
+      deletebtn.innerHTML = '<img src="./img-icon/close-btn.svg">';
+      list.appendChild(deletebtn);
+      showlist.appendChild(list);
+      input.value = "";
+      showlist.style.display = "block";
+      showlist.style.display = "flex";
+      clsform.style.display = 'none'
+      inpdata.style.border = 'none'
+    }
+  }
+  var liremove = document.querySelectorAll('.remove-btn')
+  for(var i = 0; i < liremove.length; i++){
+    liremove[i].onclick=function(){
+      this.parentNode.remove()
+      if(showlist.childElementCount == 0){
+        showlist.style.display = 'none'
+        clsform.style.display = 'block'
+        inpdata.style.border = '1px solid #C4C4C4'
+      }
+    }
+  }
+});
+
 
 btn.addEventListener('click', () => {
-  showlist.style.display = 'block'
-  showlist.style.display = 'flex'
+  clsform.style.display = 'block'
+  inpdata.style.border = '1px solid #C4C4C4'
+});
+
+remove.addEventListener('click', () => {
   input.value = ''
 })
-input.addEventListener('keyup', (e) => {
-  if(e.keyCode == 13){
-   input.value = ''
-   clsform.style.display = 'none'
-   paragraph.textContent = input.value 
-  }
 
-})
-  
- 
-remove.addEventListener('click', () => {
-  input.value=''
-})
 
