bf0e1d9a2571857854f1d838e0fab51769e3862b
Rashad <rashadquluzade@gmail.com>
2022-04-17 00:59:21 +0400

    Project

--- login.js
+++ login.js
@@ -4,57 +4,129 @@ var showlist = document.querySelector(".list");
 var remove = document.querySelector(".cls");
 var paragraph = document.querySelector(".list p");
 var clsform = document.querySelector("#cls-form");
-var inpdata = document.querySelector('.inp-data')
-
+var inpdata = document.querySelector(".inp-data");
+var arr = [];
+var sortButton = document.querySelector(".sorting");
 
 input.addEventListener("keyup", (e) => {
   if (e.keyCode == 13) {
-    if(input.value.length == 0){
-      alert("Enter the task")
-      clsform.style.display = 'block'
-      inpdata.style.border = '1px solid #C4C4C4'
-    }
-    else  {
-      const list = document.createElement("div");
-      list.classList.add("todolist");
+    if (input.value.length == 0) {
+      alert("Enter the task");
+      clsform.style.display = "block";
+      inpdata.style.border = "1px solid #C4C4C4";
+    } else {
       const addli = document.createElement("li");
       addli.classList.add("showli");
       addli.innerHTML = input.value;
-      list.appendChild(addli);
       const deletebtn = document.createElement("button");
       deletebtn.classList.add("remove-btn");
-      deletebtn.innerHTML = '<img src="./img-icon/close-btn.svg">';
-      list.appendChild(deletebtn);
-      showlist.appendChild(list);
+      addli.appendChild(deletebtn);
+      showlist.appendChild(addli);
       input.value = "";
       showlist.style.display = "block";
       showlist.style.display = "flex";
-      clsform.style.display = 'none'
-      inpdata.style.border = 'none'
+      clsform.style.display = "none";
+      inpdata.style.border = "none";
+      addli.setAttribute('draggable' ,true)
     }
   }
-  var liremove = document.querySelectorAll('.remove-btn')
-  for(var i = 0; i < liremove.length; i++){
-    liremove[i].onclick=function(){
-      this.parentNode.remove()
-      if(showlist.childElementCount == 0){
-        showlist.style.display = 'none'
-        clsform.style.display = 'block'
-        inpdata.style.border = '1px solid #C4C4C4'
+  var liremove = document.querySelectorAll(".remove-btn");
+  for (var i = 0; i < liremove.length; i++) {
+    liremove[i].onclick = function () {
+      this.parentNode.remove();
+      if (showlist.childElementCount == 0) {
+        showlist.style.display = "none";
+        clsform.style.display = "block";
+        inpdata.style.border = "1px solid #C4C4C4";
+      }
+    };
+  }
+});
+
+btn.addEventListener("click", () => {
+  clsform.style.display = "block";
+  inpdata.style.border = "1px solid #C4C4C4";
+});
+remove.addEventListener('mouseover', () => {
+  remove.style.background = `url(./img-icon/close-btn-hov.svg)`
+  remove.style.backgroundRepeat = 'no-repeat'
+  remove.style.backgroundPosition = 'center'
+})
+remove.addEventListener('mouseleave', () => {
+  remove.style.background = `url(./img-icon/close-btn.svg)`
+})
+remove.addEventListener("click", () => {
+  input.value = "";
+});
+
+
+
+
+let isTrue = true
+sortButton.onclick = function(){
+    if(isTrue){
+      isTrue = false
+      sortButton.style.background = 'url(./img-icon/sorted.svg)'
+      sortButton.style.width = '25px'
+      sortButton.style.height = '15px'
+      sortButton.style.border = 'none' 
+      sortButton.onmouseover = function(){
+        sortButton.style.background = `url(./img-icon/sorted.svg)`
+        sortButton.style.transition = '0.3s'
+      }
+      sortButton.onmouseout = function(){
+        sortButton.style.background = `url(./img-icon/sort.svg)`
+        sortButton.style.transition = '0.3s'
       }
     }
-  }  
+    else{
+      isTrue = true
+      sortButton.style.background = 'url(./img-icon/reverse-sorted.svg)'
+      sortButton.style.width = '25px'
+      sortButton.style.height = '15px'
+      sortButton.style.border = 'none'
+      sortButton.onmouseover = function(){
+        sortButton.style.background = `url(./img-icon/reverse-sorted.svg)`
+        sortButton.style.transition = '0.3s'
+      }
+      sortButton.onmouseout = function(){
+        sortButton.style.background = `url(./img-icon/reverse-sort.svg)`
+        sortButton.style.transition = '0.3s'
+      }
+    }
 
-});
+  var list, i, switching, b, shouldSwitch, dir, switchcount = 0;
+  switching = true;
+  dir = "asc"; 
+  while (switching) {
+    switching = false;
+    b = showlist.getElementsByTagName("LI");
+    for (i = 0; i < (b.length - 1); i++) {
+      shouldSwitch = false;
+      if (dir == "asc") {
+        if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (b[i].innerHTML.toLowerCase() < b[i + 1].innerHTML.toLowerCase()) {
+          shouldSwitch= true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+      switching = true;
+      switchcount ++;
+    } else {
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
 
-
-btn.addEventListener('click', () => {
-  clsform.style.display = 'block'
-  inpdata.style.border = '1px solid #C4C4C4'
-});
-
-remove.addEventListener('click', () => {
-  input.value = ''
-})
 
 
